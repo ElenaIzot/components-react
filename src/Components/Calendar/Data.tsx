b02a23d3ae41b7487847403a6bb0a1e5358865ab
Elena Izotova <elm57650@gmail.com>
2021-07-06 17:32:23 +0700

    separation into components

--- src/Components/Calendar/Data.tsx
+++ src/Components/Calendar/Data.tsx
@@ -1,15 +1,4 @@
 
-export function getDaysWeek(): JSX.Element[] {
-    const DAYS_IN_WEEK: string[] = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-
-    const renderedlistDaysWeek: JSX.Element[] = DAYS_IN_WEEK.map((day: string, index: number) =>
-        <li className="datepicker__day-week" key={index}>
-            {day}
-        </li>);
-
-    return renderedlistDaysWeek;
-};
-
 export function getDaysForCalendar(year: number, month: number): Date[] {
     const firstDayOfMonth: Date = new Date(year, month, 1);
     const dayOfWeekForFirstDay: number = firstDayOfMonth.getDay();
@@ -23,30 +12,12 @@ export function getDaysForCalendar(year: number, month: number): Date[] {
         result.push(day);
     }
 
-        for (let offsetDays = 1; offsetDays < dayOfWeekForFirstDay; ++offsetDays) {
+    
+    for (let offsetDays = 1; offsetDays < dayOfWeekForFirstDay; ++offsetDays) {
         const day = new Date(firstDayOfMonth);
         day.setDate(day.getDate() - offsetDays);
         result.unshift(day);
     }
-    
-    console.log(result)
+ 
     return result
-}
-
-
-
-// export function getDaysArray(year: number, month: number): JSX.Element[] {
-//     const date = new Date(year, month - 1, 1);
-//     const result = [];
-
-//     while (date.getMonth() == month - 1) {
-//         result.push(date.getDate());
-//         date.setDate(date.getDate() + 1);
-//     }
-
-//     let renderedlistDays = result.map((day, index) =>
-//         <li className="datepicker__day" key={index}>{day}</li>
-//     )
-//     return renderedlistDays;
-// };
-
+};
--- src/Components/Calendar/Datepicker.tsx
+++ src/Components/Calendar/Datepicker.tsx
@@ -1,21 +1,12 @@
 import { useState } from "react";
-import { getDaysWeek, getDaysForCalendar } from "./Data";
-
-const DAYS_IN_MONTH: {} = { 'Январь': 31, 'Февраль': 28, 'Март': 31, 'Апрель': 30, 'Май': 31, 'Июнь': 30, 'Июль': 31, 'Август': 31, 'Сентябрь': 30, 'Октябрь': 31, 'Ноябрь': 30, 'Декабрь': 31, }
-const MONTHS = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь',];
+import { getDaysForCalendar } from "./Data";
 
 function Datepicker(): JSX.Element {
     const d: Date = new Date();
     let today = d.toLocaleDateString();
 
     let [visible, setVisible] = useState(false);
-    let [currentYear, setYear] = useState(d.getFullYear());
-    let [currentMonth, setMonth] = useState(d.getMonth());
-    let [currentDate, setDay] = useState(d.getDate());      
-    let [value, setValue] = useState(today);
-    let [selectedDate, setSelectedDate] = useState(currentDate);
-   
-    const className = "datepicker__day_active-month" + (value ? "datepicker__day_selected-day" : "");
+    let [selectedDate, setSelectedDate] = useState(today);
 
     function showItems(): void {
         setVisible(true);
@@ -25,6 +16,49 @@ function Datepicker(): JSX.Element {
         setVisible(false);
     };
 
+    if (visible == true) {
+        return (
+            <div className="wrap">
+                <p className="title">Datepicker</p>
+                <div className="datepicker" onClick={closeItems}>
+                    <div className="datepicker__current-item">
+                        {today}
+                    </div>
+                </div>
+                <div className="datepicker__datepicker-visible">
+                    <Calendar data={d} />
+                </div>
+            </div>)
+    } else {
+        return (
+            <div className="wrap">
+                <p className="title">Datepicker</p>
+                <div className="datepicker" onClick={showItems}>
+                    <div className="datepicker__current-item">{selectedDate}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+function Calendar(props: any): JSX.Element {
+    
+    const MONTHS: string[] = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+    let today: string = props.data.toLocaleDateString();
+    let [currentYear, setYear] = useState(props.data.getFullYear());//сделать замену года при переходе на другой год
+    let [selectedDate, setSelectedDate] = useState(today);
+    let [currentMonth, setMonth] = useState(props.data.getMonth());
+
+    let monthName: string = '';
+    for (let i = 0; i < MONTHS.length; i++) {
+        if (i == currentMonth) {
+            monthName = MONTHS[i];
+        }
+    }
+
+    const renderedMonth: string = monthName[0].toUpperCase() + monthName.slice(1);
+
     function openPreviousMonth(): void {
         setMonth(currentMonth - 1)
     };
@@ -33,117 +67,61 @@ function Datepicker(): JSX.Element {
         setMonth(currentMonth + 1)
     };
 
-    let month: string = '';
-    for (let i = 0; i < MONTHS.length; i++) {
-        if (i == currentMonth) {
-            month = MONTHS[i];
-        }
-    }
-    const renderedMonth: string = month[0].toUpperCase() + month.slice(1);
-    const renderedlistDaysWeek: JSX.Element[] = getDaysWeek();
-
     const listDays: Date[] = getDaysForCalendar(currentYear, currentMonth);
-    const renderedlistDays = listDays.map((day, index) => {
+
+    const renderedlistDays: JSX.Element[] = listDays.map((day, index) => {
         if (day.getMonth() == currentMonth) {
-            if (day.getDate() == currentDate) {
-                return <li className="datepicker__day_active-month datepicker__day_current-day" onClick={() => setValue(day.toLocaleDateString())} key={index}>{day.getDate()}</li>
+            if (day.getDate() == currentMonth) {
+                return <li className="datepicker__day_active-month datepicker__day_current-day" onClick={() => setSelectedDate(day.toLocaleDateString())} key={index}>{day.getDate()}</li>
             } else {
-                return <li className="datepicker__day_active-month" onClick={() => setValue (day.toLocaleDateString())} key={index} >{day.getDate()}</li>
+                return <li className="datepicker__day_active-month" onClick={() => setSelectedDate(day.toLocaleDateString())} key={index} >{day.getDate()}</li>
             }
         } else {
-            return <li className="datepicker__day_inactive-month" onClick={() => setValue (day.toLocaleDateString())} key={index}>{day.getDate()}</li>
+            return <li className="datepicker__day_inactive-month" onClick={() => setSelectedDate(day.toLocaleDateString())} key={index}>{day.getDate()}</li>
         }
-    }
+    });
 
-    )
+    // const renderedSelectedwData = <div className="datepicker__current-item">{selectedDate}</div>;
 
-    if (visible == true) {
-        return (
-            <div className="wrap">
-                <p className="title">Datepicker</p>
-                <div className="datepicker">
-                    <div className="datepicker__current-item">{value}</div>
-                    <button className="btn-arrow datepicker__control" onClick={closeItems}><span className="btn-arrow__arrow-up"></span></button>
-                </div>
-                <div className="datepicker__datepicker-visible">
-                    <div className="datepicker__slider">
-                        <button className="btn-arrow datepicker__control" onClick={openPreviousMonth}><span className="btn-arrow__arrow-left"></span></button>
-                        <p className="datepicker__title"> {renderedMonth} {currentYear}</p>
-                        <button className="btn-arrow datepicker__control" onClick={openNextMonth}><span className="btn-arrow__arrow-right"></span></button>
-                    </div>
-                    <div className="datepicker__calendar">
-                        <ul className="datepicker__list">
-                            {renderedlistDaysWeek}
-                        </ul>
-                        <ul className="datepicker__days">
-                            {renderedlistDays}
-                        </ul>
-                    </div>
-                    {/* <Button></Button> */}
-                </div>
-            </div>)
-    } else {
-        return (
-            <div className="wrap">
-                <p className="title">Datepicker</p>
-                <div className="datepicker">
-                    <div className="datepicker__current-item">{value}</div>
-                    <button className="btn-arrow datepicker__control" onClick={showItems}><span className="btn-arrow__arrow-down"></span></button>
-                </div>
-            </div>
-        )
-    }
-}
-
-function Button() {
-    let [value, setValue] = useState(false);
-    let [choice, setСhoice] = useState('Data');
-
-    function cancelAction(): void {
-        setValue(true);
-    };
-
-    function chooseDate(): void {
-        setСhoice('Другая дата');
-    };
+    ShowSelectedData(selectedDate);
 
     return (
-        <div className="datepicker__btn-action">
-            <button className="btn datepicker__btn-action" onClick={cancelAction}>Cancel</button>
-            <button className="btn datepicker__btn-action" onClick={chooseDate}>Choose Date</button>
-        </div>
+        <>
+            {selectedDate}
+            <div className="datepicker__slider">
+                <button className="btn-arrow datepicker__control" onClick={openPreviousMonth}><span className="btn-arrow__arrow-left"></span></button>
+                <p className="datepicker__title">{renderedMonth} {currentYear}</p>
+                <button className="btn-arrow datepicker__control" onClick={openNextMonth}><span className="btn-arrow__arrow-right"></span></button>
+            </div>
+            <DaysWeek />
+            <div className="datepicker__calendar">
+                <ul className="datepicker__days">
+                    {renderedlistDays}
+                </ul>
+            </div>
+
+        </>
     )
 }
 
-// function Transition(){
-//     let [visible, setVisible] = useState(false);
+function DaysWeek(): JSX.Element {
+    const DAYS_IN_WEEK: string[] = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+    const renderedlistDaysWeek = DAYS_IN_WEEK.map((day: string, index: number) => {
+        return <li className="datepicker__day-week" key={index}>
+            {day}
+        </li>
+    });
 
-//     function showItems(): void {
-//         setVisible(true);
-//     };
+    return <ul className="datepicker__list">{renderedlistDaysWeek}</ul>
+}
 
-//     function closeItems(): void {
-//         setVisible(false);
-//     };
-//         if (visible == true) {
-//             return <button className="btn-arrow datepicker__control" onClick={closeItems}><span className="btn-arrow__arrow-up"></span></button>
-//         } else {
-//             return <button className="btn-arrow datepicker__control" onClick={showItems}><span className="btn-arrow__arrow-down"></span></button>
-//         }
-// }
+function ShowSelectedData(date: string): JSX.Element {
+    console.log(date)
+    console.log(typeof(date))
+    
+    const renderedSelectedData:JSX.Element = <div className="datepicker__current-item">{{date}}</div>;
 
-// const DATES =  years: [2017, 2018, 2019, 2020, 2021],};
-
+    return renderedSelectedData;
+}
 
 export default Datepicker;
-
-// const renderedlistDaysWeek: JSX.Element[] = DAYES_IN_WEEK.map((day: string, index: number) =>
-    //     <li className="datepicker__day-week" key={index} onClick={(e) => { setValue(value = day) }}>
-    //         {day}
-    //     </li>);
-
-        // const month = d.toLocaleString('ru', { month: 'long' })
-    // const renderedMonth = month[0].toUpperCase() + month.slice(1);
-
-     // let month: number = 12;
-    // let mon: number = month - 1; // месяцы в JS идут от 0 до 11, а не от 1 до 12
